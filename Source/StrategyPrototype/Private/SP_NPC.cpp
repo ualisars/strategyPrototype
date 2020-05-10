730bc1763ed3959d08f69ec79a8cced45c313308
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2020-05-10 13:07:01 +0300

    Setup NPC movement to towns

---
+++ Source/StrategyPrototype/Private/SP_NPC.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SP_NPC.h"
+
+// Sets default values
+ASP_NPC::ASP_NPC()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	FloatingPawnMovementComp = CreateAbstractDefaultSubobject<UFloatingPawnMovement>(TEXT("FloatingPawnMovementComp"));
+	FloatingPawnMovementComp->MaxSpeed = 100.0f;
+	FloatingPawnMovementComp->Acceleration = 0.0f;
+	FloatingPawnMovementComp->Deceleration = 0.0f;
+	MeshComp->SetCanEverAffectNavigation(false);
+	RootComponent = MeshComp;
+}
+
+void ASP_NPC::GetAllTowns()
+{
+	if (Towns.Num() == 0)
+	{
+		TArray<AActor*> FoundTowns;
+		UGameplayStatics::GetAllActorsWithTag(GetWorld(), "Town", FoundTowns);
+		UE_LOG(LogTemp, Warning, TEXT("Number of Found towns is %i"), FoundTowns.Num());
+		Towns = FoundTowns;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Number of towns is %i"), Towns.Num());
+}
+
+// Called when the game starts or when spawned
+void ASP_NPC::BeginPlay()
+{
+	Super::BeginPlay();
+	GetAllTowns();
+	if (Towns.Num() != 0)
+	{
+		NPC_MoveToActor(Towns[0]);
+	}
+}
+
+// Called every frame
+void ASP_NPC::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASP_NPC::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/StrategyPrototype/Public/SP_NPC.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/FloatingPawnMovement.h" 
+#include "SP_NPC.generated.h"
+
+UCLASS()
+class STRATEGYPROTOTYPE_API ASP_NPC : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASP_NPC();
+
+	void GetAllTowns();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	UFloatingPawnMovement* FloatingPawnMovementComp;
+
+	TArray<AActor*> Towns;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void NPC_MoveToLocation(const FVector& Location);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void NPC_MoveToActor(AActor* Town);
+
+};
