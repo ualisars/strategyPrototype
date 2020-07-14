97242755737346c322f9004d388275e68b6c2bee
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2020-07-14 18:51:27 +0300

    update game date at a tick

--- Source/StrategyPrototype/Private/SP_GameMode.cpp
+++ Source/StrategyPrototype/Private/SP_GameMode.cpp
@@ -1,9 +1,72 @@
 #include "SP_GameMode.h"
 
+void ASP_GameMode::SetClockwork(float DeltaTime)
+{
+	float ClockworkUnit = Clockwork + (DeltaTime / TimeUnit);
+	int hours = 24;
+	Clockwork = fmod(ClockworkUnit, hours);
+	DayTick = ClockworkUnit / hours;
+}
+
+void ASP_GameMode::Clock()
+{
+	int SecondsInHour = 3600;
+	int MinutesInHour = 60;
+	float TimeUnit = (Clockwork * SecondsInHour) / MinutesInHour;
+	Seconds = floor(fmod(TimeUnit, MinutesInHour));
+	float HoursAndMinutes = TimeUnit / MinutesInHour;
+	Minutes = floor(fmod(HoursAndMinutes, MinutesInHour));
+	int HoursInDay = 24;
+	float DaysAndHours = HoursAndMinutes / MinutesInHour;
+	Hours = floor(fmod(DaysAndHours, HoursInDay));
+
+	GameTime[0] = Hours;
+	GameTime[1] = Minutes;
+	GameTime[2] = Seconds;
+}
+
+void ASP_GameMode::Calendar()
+{
+	if (DayTick)
+	{
+		Day += DayTick;
+		if (Day > 26)
+		{
+			Day = 1;
+			Month += 1;
+		}
+
+		if (Month > 15)
+		{
+			Month = 1;
+			Year += 1;
+		}
+
+		GameDate[0] = Day;
+		GameDate[1] = Month;
+		GameDate[2] = Year;
+	}
+}
+
 ASP_GameMode::ASP_GameMode()
 {
+	PrimaryActorTick.bCanEverTick = true;
+	GameTime.SetNum(3);
+	GameDate = { 1, 1, 1 };
+}
+
+void ASP_GameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SetClockwork(DeltaTime);
+
+	Clock();
+
+	Calendar();
 }
 
 void ASP_GameMode::BeginPlay()
 {
+	Super::BeginPlay();
 }
--- Source/StrategyPrototype/Public/SP_GameMode.h
+++ Source/StrategyPrototype/Public/SP_GameMode.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include <math.h>
 #include "SP_GameMode.generated.h"
 
 UCLASS()
@@ -9,8 +10,24 @@ class STRATEGYPROTOTYPE_API ASP_GameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
+	float TimeUnit = 1.0f;
+	float Clockwork = 0.0f;
+	int Seconds = 0, Minutes = 0, Hours = 0;
+	TArray<int> GameTime;
+	int Day = 1, Month = 1, Year = 1;
+	TArray<int> GameDate;
+	int DayTick;
+
+	void SetClockwork(float DeltaTime);
+
+	void Clock();
+
+	void Calendar();
+
 public:
 	ASP_GameMode();
+
+	virtual void Tick(float DeltaTime) override;
 
 protected:
 	void BeginPlay() override;
