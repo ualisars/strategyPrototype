0d79c23c6f56ccbacafbaee87c8ed43d12a93a7a
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2020-07-18 19:08:49 +0300

    remove time management from game mode

--- Source/StrategyPrototype/Private/SP_GameMode.cpp
+++ Source/StrategyPrototype/Private/SP_GameMode.cpp
@@ -1,64 +1,11 @@
 #include "SP_GameMode.h"
 
-void ASP_GameMode::SetClockwork(float DeltaTime)
-{
-	float ClockworkUnit = Clockwork + (DeltaTime / TimeUnit);
-	int hours = 24;
-	Clockwork = fmod(ClockworkUnit, hours);
-	DayTick = ClockworkUnit / hours;
-}
-
-void ASP_GameMode::Clock()
-{
-	int SecondsInHour = 3600;
-	int MinutesInHour = 60;
-	float TimeUnit = (Clockwork * SecondsInHour) / MinutesInHour;
-	Minutes = floor(fmod(TimeUnit, MinutesInHour));
-	float HoursAndMinutes = TimeUnit / MinutesInHour;
-	Hours = floor(fmod(HoursAndMinutes, MinutesInHour));
-	
-	GameTime[0] = Hours;
-	GameTime[1] = Minutes;
-}
-
-void ASP_GameMode::Calendar()
-{
-	if (DayTick)
-	{
-		Day += DayTick;
-		if (Day > 26)
-		{
-			Day = 1;
-			Month += 1;
-		}
-
-		if (Month > 15)
-		{
-			Month = 1;
-			Year += 1;
-		}
-
-		GameDate[0] = Day;
-		GameDate[1] = Month;
-		GameDate[2] = Year;
-	}
-}
-
 ASP_GameMode::ASP_GameMode()
-{
-	GameTime.SetNum(2);
-	GameDate = { 1, 1, 1 };
-}
+{}
 
 void ASP_GameMode::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	SetClockwork(DeltaTime);
-
-	Clock();
-
-	Calendar();
 }
 
 void ASP_GameMode::BeginPlay()
--- Source/StrategyPrototype/Public/SP_GameMode.h
+++ Source/StrategyPrototype/Public/SP_GameMode.h
@@ -10,28 +10,10 @@ class STRATEGYPROTOTYPE_API ASP_GameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
-	float TimeUnit = 2.5f;
-	float Clockwork = 0.0f;
-	int Hours = 0, Minutes = 0;
-	int Day = 1, Month = 1, Year = 1;
-	int DayTick;
-
-	void SetClockwork(float DeltaTime);
-
-	void Clock();
-
-	void Calendar();
-
 public:
 	ASP_GameMode();
 
 	virtual void Tick(float DeltaTime) override;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	TArray<int> GameTime;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	TArray<int> GameDate;
 
 protected:
 	void BeginPlay() override;
