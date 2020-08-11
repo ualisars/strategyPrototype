c6879d76836a0a915648d0cb7dcae22f27fa8ebe
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2020-08-11 18:42:49 +0300

    add safety to Cell

--- Source/StrategyPrototype/Private/Utils/GridSystem/SP_Cell.cpp
+++ Source/StrategyPrototype/Private/Utils/GridSystem/SP_Cell.cpp
@@ -7,6 +7,35 @@ SP_Cell::SP_Cell()
 SP_Cell::SP_Cell(float X, float Y): mX(X), mY(Y)
 {}
 
+void SP_Cell::SetSafety(int Safety)
+{
+	if (Safety >= MinSafety && Safety <= MaxSafety)
+	{
+		mSafety = Safety;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Unable to set safety, incorrect value passed"));
+	}
+}
+
+void SP_Cell::AddSafety(int Value)
+{
+	if (Value >= MinSafety && Value <= MaxSafety)
+	{
+		mSafety += Value;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Unable to set safety, incorrect value passed"));
+	}
+}
+
+int SP_Cell::GetSafety()
+{
+	return mSafety;
+}
+
 SP_Cell::~SP_Cell()
 {
 }
--- Source/StrategyPrototype/Public/Utils/GridSystem/SP_Cell.h
+++ Source/StrategyPrototype/Public/Utils/GridSystem/SP_Cell.h
@@ -5,10 +5,21 @@
 class STRATEGYPROTOTYPE_API SP_Cell
 {
 	float mX, mY;
+
+	short mSafety = 0;
+
+	short MinSafety = -5;
+	short MaxSafety = 5;
 public:
 	SP_Cell();
 
 	SP_Cell(float X, float Y);
+
+	void SetSafety(int Safety);
+
+	void AddSafety(int Value);
+
+	int GetSafety();
 
 	~SP_Cell();
 };
