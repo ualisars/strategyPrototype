64d9422bc7836530a8770d226f41fa8ba5553873
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2020-08-13 17:38:44 +0300

    use grid system in avoid enemy task

--- Source/StrategyPrototype/Private/AI/NPC/Tasks/SP_AvoidStrongerEnemyTask.cpp
+++ Source/StrategyPrototype/Private/AI/NPC/Tasks/SP_AvoidStrongerEnemyTask.cpp
@@ -1,7 +1,10 @@
 #include "AI/NPC/Tasks/SP_AvoidStrongerEnemyTask.h"
 #include "SP_AIController.h"
 #include "SP_NPC.h"
+#include "Runtime/NavigationSystem/Public/NavigationSystem.h"
+#include "BehaviorTree/BlackboardComponent.h"
 #include "Utils/GridSystem/SP_GridSystem.h"
+#include "AI/SP_BlackboardKeys.h"
 
 USP_AvoidStrongerEnemyTask::USP_AvoidStrongerEnemyTask(const FObjectInitializer& ObjectInitializer)
 {
@@ -13,5 +16,21 @@ EBTNodeResult::Type USP_AvoidStrongerEnemyTask::ExecuteTask(UBehaviorTreeCompone
 	ASP_AIController* AIController = Cast<ASP_AIController>(OwnerComp.GetAIOwner());
 	ASP_NPC* NPC = Cast<ASP_NPC>(AIController->GetPawn());
 
-	return EBTNodeResult::Type();
+	SP_GridSystem GridSystem = SP_GridSystem(NPC, GridWidth, GridHeight, CellSize);
+	GridSystem.ConstructGrid(NPC);
+
+	SP_Cell SafestCell = GridSystem.GetSafestCell();
+
+	const UNavigationSystemV1* NavSys = UNavigationSystemV1::GetCurrent(GetWorld());
+
+	FNavLocation Location;
+	FVector Origin = FVector(SafestCell.GetCenterX(), SafestCell.GetCenterY(), 0.0f);
+
+	if (NavSys->GetRandomPointInNavigableRadius(Origin, CellSize, Location, nullptr))
+	{
+		AIController->GetBlackboard()->SetValueAsVector(BBKeys::TargetLocation, Location.Location);
+	}
+
+	FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	return EBTNodeResult::Succeeded;
 }
--- Source/StrategyPrototype/Public/AI/NPC/Tasks/SP_AvoidStrongerEnemyTask.h
+++ Source/StrategyPrototype/Public/AI/NPC/Tasks/SP_AvoidStrongerEnemyTask.h
@@ -4,14 +4,22 @@
 #include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
 #include "SP_AvoidStrongerEnemyTask.generated.h"
 
-UCLASS()
+UCLASS(Blueprintable)
 class STRATEGYPROTOTYPE_API USP_AvoidStrongerEnemyTask : public UBTTask_BlackboardBase
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid", meta = (AllowPrivateAccess = "true"))
+	int GridWidth = 800;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid", meta = (AllowPrivateAccess = "true"))
+	int GridHeight = 800;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid", meta = (AllowPrivateAccess = "true"))
+	int CellSize = 50;
 
 public:
 	USP_AvoidStrongerEnemyTask(const FObjectInitializer& ObjectInitializer);
 
 	EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory);
-	
 };
