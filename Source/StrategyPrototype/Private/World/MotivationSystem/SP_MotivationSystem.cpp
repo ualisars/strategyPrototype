4e567a1ef80a6a409692110dda501d741893f276
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2020-09-02 18:13:37 +0300

    create Motivation System

---
+++ Source/StrategyPrototype/Private/World/MotivationSystem/SP_MotivationSystem.cpp
@@ -0,0 +1,7 @@
+#include "World/MotivationSystem/SP_MotivationSystem.h"
+
+FSP_Motivation  USP_MotivationSystem::AssignMotivation()
+{
+	return FSP_Motivation();
+}
+
---
+++ Source/StrategyPrototype/Public/World/MotivationSystem/SP_MotivationSystem.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SP_MotivationSystem.generated.h"
+
+UENUM(BlueprintType)
+enum class SP_MotivationObjective : uint8
+{
+	Domination,
+	Protection,
+	Revenge,
+	Wealth
+};
+
+USTRUCT(BlueprintType)
+struct FSP_Motivation
+{
+	GENERATED_BODY()
+
+	AActor* TargetActor = nullptr;
+
+	SP_MotivationObjective Objective;
+
+	unsigned char Passion = 0;
+
+	FSP_Motivation() {}
+
+	FSP_Motivation(AActor* Actor, SP_MotivationObjective Objective, unsigned short Passion)
+		: TargetActor(Actor), Objective(Objective), Passion(Passion) {}
+};
+
+UCLASS()
+class STRATEGYPROTOTYPE_API USP_MotivationSystem : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	static FSP_Motivation AssignMotivation();
+};
