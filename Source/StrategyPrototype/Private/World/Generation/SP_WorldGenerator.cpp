605cb37cd9402f47749acbd18a01ada2f81b8e60
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2020-10-15 18:32:20 +0300

    move actor spawner and spawn checker to world generator

--- Source/StrategyPrototype/Private/World/Generation/SP_WorldGenerator.cpp
+++ Source/StrategyPrototype/Private/World/Generation/SP_WorldGenerator.cpp
@@ -1,10 +1,72 @@
 #include "World/Generation/SP_WorldGenerator.h"
+#include "World/Spawn/SP_ActorSpawner.h"
+#include "World/Spawn/SP_SpawnChecker.h"
+
+
+void ASP_WorldGenerator::SetActorSpawner()
+{
+	USP_ActorSpawner* ActorSpawner = NewObject<USP_ActorSpawner>(USP_ActorSpawner::StaticClass());
+
+	if (ActorSpawner)
+	{
+		mActorSpawner = ActorSpawner;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to create Actor Spawner in World Generator"));
+	}
+}
+
+void ASP_WorldGenerator::SetSpawnChecker()
+{
+	USP_SpawnChecker* SpawnChecker = NewObject<USP_SpawnChecker>(USP_SpawnChecker::StaticClass());
+
+	if (SpawnChecker)
+	{
+		mSpawnChecker = SpawnChecker;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to create Spawn Checker in World Generator"));
+	}
+}
+
+void ASP_WorldGenerator::InitWorld()
+{
+	FVector Location = FVector(-520.0f, 130.0f, 58.0f);
+	FRotator Rotation = FRotator(0.0f, -90.0f, 0.0f);
+
+	mActorSpawner->Init(GetWorld());
+	mSpawnChecker->Init(GetWorld());
+
+	mActorSpawner->SpawnPlayer(PlayerClass, CameraClass, Location, Rotation);
+
+	FVector NPCLocation = FVector(0.0f, 0.0f, 0.0f);
+	FRotator NPCRotation = FRotator(0.0f, -90.0f, 0.0f);
+
+	mActorSpawner->SpawnNPC(NPCClass, NPCLocation, NPCRotation);
+}
 
 ASP_WorldGenerator::ASP_WorldGenerator()
-{}
+{
+	SetActorSpawner();
+	SetSpawnChecker();
+}
 
 void ASP_WorldGenerator::BeginPlay()
 {
-	Super::BeginPlay();	
+	Super::BeginPlay();
+
+	InitWorld();
+}
+
+USP_ActorSpawner* ASP_WorldGenerator::GetActorSpawner() const
+{
+	return mActorSpawner;
+}
+
+USP_SpawnChecker* ASP_WorldGenerator::GetSpawnChecker() const
+{
+	return mSpawnChecker;
 }
 
--- Source/StrategyPrototype/Public/World/Generation/SP_WorldGenerator.h
+++ Source/StrategyPrototype/Public/World/Generation/SP_WorldGenerator.h
@@ -8,10 +8,34 @@ UCLASS()
 class STRATEGYPROTOTYPE_API ASP_WorldGenerator : public AActor
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<class ASP_BaseCharacter> NPCClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<class ASP_BaseCharacter> PlayerClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<class ASP_GameCamera> CameraClass;
+
+	class USP_ActorSpawner* mActorSpawner;
+
+	class USP_SpawnChecker* mSpawnChecker;
+
+	void SetActorSpawner();
+
+	void SetSpawnChecker();
+
+	void InitWorld();
 	
 public:	
 	ASP_WorldGenerator();
 
 protected:
 	virtual void BeginPlay() override;
+
+public:
+	USP_ActorSpawner* GetActorSpawner() const;
+
+	class USP_SpawnChecker* GetSpawnChecker() const;
 };
